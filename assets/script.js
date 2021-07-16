61cf601157a05d81837d5d37ee88ea378a27e097
Vankoekelberg <Mathis>
2021-07-16 11:55:16 +0200

    edit

--- assets/script.js
+++ assets/script.js
@@ -1,107 +1,112 @@
 
-
-// var canvas = document.getElementById("game");
-// var ctx = canvas.getContext("2d");
-// var image = document.getElementById("source");
-// ctx.lineWidth = "5";
-// ctx.strokeStyle = "#000";
-// ctx.canvas.width  = window.innerWidth/1.5;
-// ctx.canvas.height = window.innerHeight/1.5;
-
-// var yValShoot = 525
-// var yval = 550;
-// var xval = (canvas.width/2) - 35;
-// // var autoShoot;
-// var speed = 5000;
-// ctx.drawImage(image, xval, yval, image.width = 70, image.height = 70);
-
-
-
-// function drawShoot() {
-//     ctx.beginPath();
-//     ctx.lineTo(xval + 35, yValShoot);
-//     ctx.lineTo(xval + 35, yValShoot + 10);
-//     ctx.closePath();
-// }
-// function shipShoot() {
-//     // document.addEventListener('keydown', function(event) {
-//         // if(event.keyCode === 32) {
-//         //     //space
-//         //     console.log('xval: ', xval);
-//         //     console.log("shoot");
-//         // }
-//         // autoShoot = setInterval(shoot, speed) ;
-//         // function shoot() {
-//             // ctx.clearRect(0, 0, canvas.width, canvas.height);
-//             drawShoot();
-//             // moveShip();
-//             yValShoot -= 10;
-//         // }
-//     // });
-// }
-
-// function moveShip() {
-// document.addEventListener('keydown', function(event) {
-//     if(event.keyCode === 37) { //left
-//         if(xval < 13) {
-//             xval = 13;
-//         }
-//         xval -= 10;
-//         ctx.clearRect(0, 0, canvas.width, canvas.height);
-//         ctx.drawImage(image,xval,yval, image.width = 70, image.height = 70);
-//         shipShoot();
-//         // drawShoot();
-//     }
-//     if(event.keyCode === 39) { //right
-//         if(xval > canvas.width - 90) {
-//             xval = canvas.width -90;
-//         }
-//         xval += 10;
-//         ctx.clearRect(0, 0, canvas.width, canvas.height);
-//         ctx.drawImage(image,xval,yval, image.width = 70, image.height = 70);
-//         shipShoot();
-//         // drawShoot();
-//         }
-//     });
-// }
-
-
-// setInterval(moveShip, speed);
-// setInterval(shipShoot, speed);
-// drawShoot();
-// moveShip();
-// shipShoot();
-
-
-
-
-
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
-ctx.canvas.width  = window.innerWidth/1.5;
-console.log('canvas.width: ', canvas.width);
-ctx.canvas.height = window.innerHeight/1.5;
-var image = document.getElementById("source");
 var image2 = document.getElementById("source2");
-var ballRadius = 5;
-var x = canvas.width/2;
-var y = 525;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
-var rightPressed = false;
-var leftPressed = false;
-var spacePressed = false;
+var img = document.getElementById("source");
+ctx.lineWidth = "5";
+ctx.strokeStyle = "rgb(54, 95, 156)";
+canvas.width  = window.innerWidth/1.5;
+canvas.height = window.innerHeight/1.5;
+var score = 0
+var start = false;
+var inter;
+var arrayProjectile = [];
 var yval = 550;
 var xval = (canvas.width/2) - 35;
-var yvalTarget = -70;
-var xvalTarget = (canvas.width-paddleWidth)/2; 
-// var xvalTarget = Math.floor(Math.random() * (canvas.width -100)) + 70; 
+var yvalTarget = -35;
+var xvalTarget = (canvas.width/2);
+var x = canvas.width/2;
+var y = canvas.height-75;
+var ballRadius = 5;
+var dy = -2;
+var rightPressed = false;
+var leftPressed = false;
+var target = 0;
+var arrayTarget = [];
+var paddleHeight = 10;
+var paddleWidth = 75;
+var difficulty = 0;
+var theme = 0;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keydown", function setProjectile(event) {
+    if(event.code === "ArrowUp") {
+        arrayProjectile.push([xval, y]);
+    }
+});
+
+document.getElementById("buttonStart").addEventListener("click", () => {
+    if(difficulty === 0) {
+        alert("Choice your difficulty !")
+    }
+    else {
+        if (start) {
+            document.getElementById("buttonStart").innerHTML = "Start game";
+            clearInterval(inter);
+            start = false;
+        } else {
+            document.getElementById("buttonStart").innerHTML = "Pause game";
+            inter = setInterval(draw, 10);
+            start = true;
+        }
+        if(start === true) {
+            document.getElementById("buttonDifficulty").disabled = true;
+        }
+        if(start === true) {
+            document.getElementById("buttonTheme").disabled = true;
+        }
+    }
+});
+document.getElementById("buttonReset").addEventListener("click", () => {
+    window.location.reload();
+});
+
+document.getElementById("buttonDifficulty").addEventListener("click", () => {
+    difficulty++;
+    if(difficulty === 1) {
+        document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Easy";
+    }
+    else if(difficulty === 2) {
+        document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Medium";
+    }
+    else if(difficulty === 3) {
+        document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Hard";
+    }
+    else if(difficulty === 4) {
+        document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Extrem";
+    }
+    else if(difficulty === 5) {
+        difficulty = 1;
+        document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Easy";
+    }
+});
+document.getElementById("buttonTheme").addEventListener("click", () => {
+    theme++;
+    if(theme === 1) {
+        document.getElementById("game").removeAttribute("class");
+        document.getElementById("game").setAttribute("class", "theme1");
+    }
+    else if(theme === 2) {
+        document.getElementById("game").removeAttribute("class");
+        document.getElementById("game").setAttribute("class", "theme2");
+    }
+    else if(theme === 3) {
+        document.getElementById("game").removeAttribute("class");
+        document.getElementById("game").setAttribute("class", "theme3");
+    }
+    else if(theme === 4) {
+        theme = 1;
+        document.getElementById("game").removeAttribute("class");
+        document.getElementById("game").setAttribute("class", "theme1");
+    }
+});
+
+
+for (let i = 0; i < 10; i++) {
+    arrayTarget.push(Math.floor(Math.random() * (canvas.width -35)))
+}
+
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -110,10 +115,8 @@ function keyDownHandler(e) {
     else if(e.keyCode == 37) {
         leftPressed = true;
     }
-    // else if(e.keyCode == 32) {
-    //     spacePressed = true;
-    // }
 }
+
 function keyUpHandler(e) {
     if(e.keyCode == 39) {
         rightPressed = false;
@@ -121,103 +124,82 @@ function keyUpHandler(e) {
     else if(e.keyCode == 37) {
         leftPressed = false;
     }
-    // else if(e.keyCode == 32) {
-    //     spacePressed = false;
-    // }
 }
 
-function drawTarget() {
-    ctx.drawImage(image2, xvalTarget, yvalTarget, image.width = 70, image.height = 70);
-}
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y + 15, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#c73133";
-    ctx.fill();
-    ctx.closePath();
-}
 function drawPaddle() {
-    ctx.drawImage(image, xval, yval, image.width = 70, image.height = 70);
+    ctx.drawImage(img, xval, yval, img.width = 70, img.height = 70);
 }
 
-function draw() {
+function drawTarget(a) {
+    ctx.drawImage(image2, a, yvalTarget, image2.width = 35, image2.height = 35);
+    yvalTarget += 1 * difficulty;
+}
+
+function drawProjectile() {
+    for (let i = 0; i < arrayProjectile.length; i++) {
+        if(arrayTarget[target] < arrayProjectile[i][0]+35 + 10 && arrayTarget[target] + 35 > arrayProjectile[i][0]+35 && yvalTarget < arrayProjectile[i][1] + 10 && 35 + yvalTarget > arrayProjectile[i][1]) { //shoot hit
+            ctx.clearRect(arrayTarget[target], yvalTarget, image2.width = 70, image2.height = 70);
+            arrayProjectile.splice(i, 1);
+            score++;
+            yvalTarget = -35;
+            target++;
+            drawTarget(arrayTarget[target]);
+        }
+        if (arrayProjectile.length > 0) {
+            ctx.beginPath();
+            ctx.arc(arrayProjectile[i][0]+35, arrayProjectile[i][1] + 15, ballRadius, 0, Math.PI*2);
+            ctx.fillStyle = "#c73133";
+            ctx.fill();
+            ctx.closePath();
+            arrayProjectile[i][1] = (arrayProjectile[i][1] + dy);
+
+            if (arrayProjectile[i][1] < 0) {
+                arrayProjectile.splice(i, 1);
+            }
+        }
+    }
+}
+
+function collides() {
+    if(arrayTarget[target] < xval + 35 && arrayTarget[target] + 70 > xval && yvalTarget < yval + 70 && 35 + yvalTarget > yval) { //target hit
+        clearInterval(inter);
+        document.getElementById("winLose").innerHTML = "You Lose !";
+        document.getElementById("game").setAttribute("style", "opacity: 0.5");
+        // alert("You lose");
+    }
+    if(yvalTarget > canvas.height) {
+        clearInterval(inter);
+        document.getElementById("winLose").innerHTML = "You Lose !";
+        document.getElementById("game").setAttribute("style", "opacity: 0.5");
+        // alert("You lose 1");
+    }
+}
+
+function logScore() {
+    document.getElementById("score").innerHTML = score;
+}
+
+function win() {
+    if (score == 10) {
+        clearInterval(inter);
+        document.getElementById("winLose").innerHTML = "You win !";
+        document.getElementById("game").setAttribute("style", "opacity: 0.5");
+        // alert("You win!");
+    }
+}
+
+function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawTarget(arrayTarget[target]);
+    drawProjectile();
     drawPaddle();
-    drawBall();
     collides();
-    // document.addEventListener("keydown",(e) => {
-    //     if(e.keycode === 32) {
-    //         drawBall();
-    //         console.log("yes");
-    //     }
-    // })
+    logScore();
+    win();
     if(rightPressed && xval < canvas.width-paddleWidth) {
         xval += 4;
     }
     else if(leftPressed && xval > 0) {
         xval -= 4;
     }
-    if(yvalTarget > canvas.height) {
-        clearInterval(inter);
-        alert("You lose");
-    }
-    // else if(spacePressed) {
-    //     drawBall();
-    // }
-    
-    // x += dx;
-    y += dy;
-    yvalTarget += 2;
 }
-
-var inter = setInterval(draw, 10);
-var interTarget = setInterval(drawTarget, 10);
-
-function collides() {
-    if(xvalTarget < xval + 70 && xvalTarget + 70 > xval && yvalTarget < yval + 70 && 70 + yvalTarget > yval) { //target hit
-        clearInterval(inter);
-        alert("You lose");
-    }
-    if(xvalTarget < x + 10 && xvalTarget + 70 > x && yvalTarget < y + 10 && 70 + yvalTarget > y) { //shoot it
-        ctx.clearRect(xvalTarget, yvalTarget, image.width = 70, image.height = 70);
-        ctx.clearRect(x, y + 15, ballRadius, 0, Math.PI*2);
-        clearInterval(interTarget);
-        console.log("ta mere la commode");
-    }
-}
-
-// document.getElementById("buttonStart").addEventListener("click", () => {
-// })
-// document.getElementById("buttonReset").addEventListener("click", () => {
-// })
-        
-        
-// ctx.beginPath();
-// ctx.rect(325, 250, 14, 115);
-// ctx.rect(338, 295, 17, 44);
-// ctx.rect(354, 308, 195, 16);
-// ctx.rect(548, 295, 14, 44);
-// ctx.rect(560, 250, 17, 115);
-// ctx.rect(422, 320, 64, 15);
-// ctx.rect(390, 332, 124, 20);
-// ctx.rect(405, 352, 12, 19);
-// ctx.rect(490, 352, 12, 19);
-// ctx.rect(427, 352, 51, 8);
-// ctx.rect(400, 310, 105, -42);
-// ctx.rect(421, 270, 67, -114);
-// ctx.rect(437, 157, 37, -69);
-// ctx.rect(448, 90, 15, -23);
-// ctx.fill();
-// ctx.stroke();
-
-
-
-
-
-
-
-
-
-
-// console.log(document.addEventListener("click", event => {console.log(event.layerX)}))
-// console.log(document.addEventListener("click", event => {console.log(event.layerY)}))
